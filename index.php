381dc0982cc9a1c3a4ec6b962797ceffd0f1b0ea
Pradeep Kumar <pradeep3@users.noreply.github.com>
2016-04-29 11:43:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Kode Fidle</title>
+	<link rel="stylesheet" href="css/primary.css">
+</head>
+	<body>
+		<table>
+			<tr>
+				<td>
+					<div class="tag">HTML (Body)</div>
+					<div id="html" class="content" contenteditable></div>
+				</td>
+				<td>
+					<div class="tag">CSS</div>
+					<div id="css" class="content" contenteditable></div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="tag">JavaScript</div>
+					<div id="js" class="content" contenteditable></div>
+				</td>
+				<td>
+					<div class="tag">Output</div>
+					<iframe id="output"></iframe>
+				</td>
+			</tr>
+		</table>
+
+	<script src="js/app.js"></script>
+	</body>
+</html>
